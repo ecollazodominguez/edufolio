8ba1fdb9aaad4db1116cd694c2bcb06b9cfdbc04
Eduardo Collazo <ecollazodominguez@gmail.com>
2023-01-10 16:37:26 +0100

    Finished Header CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,23 @@ export const Navbar = () => {
       <button className="header-about" onClick={() => navigate("/")}>
         Eduardo Collazo
       </button>
+
       <nav className="header-nav">
         <button className="header-cv" onClick={() => navigate("/curriculum")}>
           Currículum
         </button>
+        <svg id="verticalLine">
+          <line x1="0" y1="60" x2="0" y2="90"></line>
+        </svg>
         <button
           className="header-project"
           onClick={() => navigate("/projects")}
         >
           Proyectos
         </button>
+        <svg id="verticalLine">
+          <line x1="0" y1="60" x2="0" y2="90"></line>
+        </svg>
         <button className="header-contact" onClick={() => navigate("/contact")}>
           Contacto
         </button>
