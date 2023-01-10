aa0e9b52b1734c55b2ab847a4d8cac4b72b533f6
Eduardo Collazo <ecollazodominguez@gmail.com>
2023-01-10 17:16:04 +0100

    finished About CSS (animations left)

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,22 @@ export const About = () => {
   const navigate = useNavigate();
   return (
     <section className="about">
-      <img src="" alt="foto"></img>
+      <img src={require("../aboutpict.webp")} alt="foto"></img>
       <article className="about-text">
         <h2>Hola</h2>
         <h4>Un poco de mí</h4>
-        <p>lorem ipsum</p>
+        <p>
+          Soy un <b>desarrollador</b> tanto de <b>web</b> como de
+          <b> aplicaciones</b>. He estudiado
+          <b> lenguajes</b> como <b>Java, Python, JS</b> y <b>frameworks </b>
+          como <b>node.JS</b> o <b>REACT</b>.
+        </p>
+        <br />
+        <p>
+          Soy una persona de
+          <b> rápido aprendizaje, curiosa y amante de la programación</b>. Te
+          animo a que veas mis <b>proyectos</b>.
+        </p>
         <menu className="about-menu">
           <button className="about-cv" onClick={() => navigate("/curriculum")}>
             Currículum
